5a7f22be919a8a581c864a46274e0b23a3a3250b
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2016-03-13 16:13:17 -0300

    Add the classes, structs and enums that is the model layer.
    Also add support for AppCode IDE.

---
+++ RxMarvel/Models/Hero.swift
@@ -0,0 +1,17 @@
+//
+//  Hero.swift
+//  RxMarvel
+//
+//  Created by Rafael Ferreira on 3/13/16.
+//  Copyright © 2016 Swift Yah. All rights reserved.
+//
+
+import Foundation
+
+struct Hero {
+    
+    let id: Int
+    let name: String
+    let thumbnail: Thumbnail
+    
+}
---
+++ RxMarvel/ViewControllers/HeroTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  RxMarvel
+//
+//  Created by Rafael Ferreira on 3/13/16.
+//  Copyright © 2016 Swift Yah. All rights reserved.
+//
+
+import UIKit
+
+/// The view controller for hero table view at `Main.storyboard`.
+class HeroTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
