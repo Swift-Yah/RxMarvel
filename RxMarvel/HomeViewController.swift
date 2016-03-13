b449b6a2daeed4e5b086ae5c428ad2e8f9c7667b
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2016-03-13 14:55:02 -0300

    Reestructure all project in logic folder.

---
+++ RxMarvel/HomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  RxMarvel
+//
+//  Created by Rafael Ferreira on 3/13/16.
+//  Copyright © 2016 Swift Yah. All rights reserved.
+//
+
+import UIKit
+
+/// The view controller for home view at `Main.storyboard`.
+public class HomeViewController: UIViewController {
+    
+}
